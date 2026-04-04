b62329305ba643bd81bf9f8a2d611b1909c054d8
coleYab <yabume123@gmail.com>
2026-04-04 10:57:03 +0000

    feat: added this things

--- src/app/dashboard/overview/page.tsx
+++ src/app/dashboard/overview/page.tsx
@@ -1,5 +1,9 @@
 import PageContainer from '@/components/layout/page-container';
+import { redirect } from 'next/navigation';
 
 export default function OverviewPage() {
-  return <PageContainer pageTitle='Overview'>{null}</PageContainer>;
+  // return <PageContainer pageTitle='Overview'>{null}</PageContainer>;
+  
+      return redirect('/dashboard/trips');
+
 }
--- src/app/dashboard/rewards/points-client.tsx
+++ src/app/dashboard/rewards/points-client.tsx
@@ -117,7 +117,7 @@ export default function MyPointsClient() {
           </div>
         </div>
 
-        <div className='grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-4'>
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
           {rewards.map((reward) => {
             const isAffordable = points >= reward.coinCost;
             const redeemedCount = redeemedCounts[reward.name] ?? 0;
